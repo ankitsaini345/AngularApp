55d2c557accce48ecfc7424856e3ca8191c8318b
ankitsaini345@gmail.com <ankitsaini345@gmail.com>
2019-10-08 00:30:44 +0530

    Navbar Examples Added

--- src/app/templates/bulma-css/bulma-css.module.ts
+++ src/app/templates/bulma-css/bulma-css.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { BulmaRoutingModule } from './bulma-routing.module';
+import { BulmaComponent } from './bulma.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent],
+  declarations: [NavbarComponent, FooterComponent, BulmaComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    BulmaRoutingModule
   ],
   exports: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    BulmaComponent,
+    BulmaRoutingModule
   ]
 })
 export class BulmaCssModule { }
